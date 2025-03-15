a1c29aa5d147c345362e25c4bd252baccd9841b5
Fatih Demirci <fathdmrc01@gmail.com>
2025-03-15 11:33:50 +0300

    İlk karakteri son karakter ile değiştirme eklendi

---
+++ İlkKarakterSonda/Program.cs
@@ -0,0 +1,13 @@
+﻿while (true)
+{
+    Console.WriteLine("Girilen kelimenin ilk harfini en sona getirir\n*************************************************\n Lütfen kelime girin:");
+    string metin = Console.ReadLine();
+    Console.WriteLine(degistir(metin));
+}
+static string degistir(string metin)
+{
+    char ilkkarakter = metin[0];
+    string kalan = metin.Substring(1);
+    kalan += ilkkarakter;
+    return kalan;
+}
