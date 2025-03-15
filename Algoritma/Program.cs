3c9cbf255197459455db7539ec625bd19399a676
Fatih Demirci <fathdmrc01@gmail.com>
2025-03-15 11:05:05 +0300

    Algoritma Projesi eklendi

---
+++ Algoritma/Program.cs
@@ -0,0 +1,12 @@
+﻿while (true)
+{
+    System.Console.WriteLine("Bu program girilen metinden , girilen indekse karşılık gelen değeri metinden siler.");
+    System.Console.WriteLine("Lütfen metin giriniz:(Metin,sayi)");
+    System.Console.WriteLine("------------------------------------------------------------");
+    string metinsayi = Console.ReadLine();
+    string[] dizi = metinsayi.Split(",");
+    string metin = dizi[0];
+    int sayi = Convert.ToInt32(dizi[1]);
+    metin = metin.Remove(sayi, 1);
+    Console.WriteLine(metin);
+}
