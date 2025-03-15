1f55893ad5aea21d3c3e154c51e4bb3a3305c2e2
Fatih Demirci <fathdmrc01@gmail.com>
2025-03-15 10:46:55 +0300

    Daire Çizme Konsol Projesi eklendi

---
+++ DaireCizme/Program.cs
@@ -0,0 +1,24 @@
+﻿while (true)
+{
+    System.Console.WriteLine("Çizilecek dairenin yarıçapını giriniz:");
+    string yaricap = Console.ReadLine();
+    if (Int32.TryParse(yaricap, out int dyaricap))
+    {
+        for (int y = -dyaricap; y <= dyaricap; y++)
+        {
+            for (int x = -dyaricap; x <= dyaricap; x++)
+            {
+                // Dairenin denklemi: x^2 + y^2 = r^2
+                if (x * x + y * y <= dyaricap * dyaricap)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
