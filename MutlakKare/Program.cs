b87583b417ddb1d5ca5f64edaf6663982f39d766
Fatih Demirci <fathdmrc01@gmail.com>
2025-03-16 10:11:46 +0300

    Mutlak Kare Alma konsol projesi

---
+++ MutlakKare/Program.cs
@@ -0,0 +1,21 @@
+﻿while (true)
+{
+    Console.WriteLine("Lütfen sayıları virgül ile ayırarak giriniz:");
+    string sayilar = Console.ReadLine();
+    string[] sayilardizi = sayilar.Split(",");
+    int farktoplam = 0;
+    int farkkaretoplam = 0;
+    for (int i = 0; i < sayilardizi.Length; i++)
+    {
+        int sayi = Convert.ToInt32(sayilardizi[i]);
+        if (sayi > 67)
+        {
+            farkkaretoplam = farkkaretoplam + (sayi - 67) * (sayi - 67);
+        }
+        else
+        {
+            farktoplam = farktoplam + (67 - sayi);
+        }
+    }
+    Console.WriteLine("67 den küçüklerin farklarının toplamı :" + farktoplam + "\n67 den büyüklerin farklarının karelerinin toplamı:" + farkkaretoplam);
+}
